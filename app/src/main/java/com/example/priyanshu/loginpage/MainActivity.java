34129f8c8f444c5f97fbe11d0844af6b76815d93
Priyanshu Gautam <priyanshugautam1998@gmail.com>
2018-10-18 18:03:13 +0530

    modifying layout of main activity

--- app/src/main/java/com/example/priyanshu/loginpage/MainActivity.java
+++ app/src/main/java/com/example/priyanshu/loginpage/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.priyanshu.loginpage;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.graphics.Color;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,29 +19,12 @@ public class MainActivity extends AppCompatActivity {
     int attempt_counter=5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         button_login = (Button)findViewById(R.id.button_login);
     }
 
-    public void checkCredentials(View view) {
-        username = ((EditText)findViewById(R.id.value_username)).getText().toString();
-        password = ((EditText)findViewById(R.id.value_password)).getText().toString();
-        if(username.equals("priyanshu")&& password.equals("abc@123"))
-        {
-            Intent intent = new Intent(this, LoggedIn.class);
-            startActivity(intent);
-        }
-        else
-        {
-            Toast.makeText(this,"Username and password is NOT correct",
-                    Toast.LENGTH_SHORT).show();
-            attempt_counter--;
-            if(attempt_counter==5)
-                button_login.setEnabled(false);
 
-        }
-    }
 }
